c55b12b67cccce9da7d16dabe714dfa5749a6698
Dai Ibrahim <33212069+Dai-Ibrahim@users.noreply.github.com>
2020-01-31 01:50:59 -0700

    Assignment Submission

---
+++ Scripts/Flee.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flee
+{
+    public GameObject target; //target
+    public Kinematic character; //char
+
+    public float maxAcceleration = 15f;
+	public Vector3 offset = new Vector3(4f,0,-4f);
+
+
+    public SteeringOutput getSteering()
+    {
+        SteeringOutput result = new SteeringOutput();
+        result.linearVelocity = (character.transform.position - target.transform.position) ;
+
+        // give full acceleration along this direction
+        result.linearVelocity.Normalize();
+        result.linearVelocity *= maxAcceleration;
+
+        result.angularVelocity = 0;
+        return result;
+    }
+}
